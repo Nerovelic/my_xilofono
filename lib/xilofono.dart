28fb2adcd23fd7250d2ecb082ed44136f0fd8a9e
Nerovelic <zack.vadhir2@hotmail.com>
2023-02-14 22:37:59 -0800

    cambio de nombre de assets/sounds/assets_note1.wav -> assets/sounds/note1.wav 	cambio de nombre de assets/sounds/assets_note2.wav -> assets/sounds/note2.wav 	cambio de nombre de assets/sounds/assets_note3.wav -> assets/sounds/note3.wav 	cambio de nombre de assets/sounds/assets_note4.wav -> assets/sounds/note4.wav 	cambio de nombre de assets/sounds/assets_note5.wav -> assets/sounds/note5.wav 	cambio de nombre de assets/sounds/assets_note6.wav -> assets/sounds/note6.wav 	cambio de nombre de assets/sounds/assets_note7.wav -> assets/sounds/note7.wav 	añadido de los botones del xilofono como tambien su sonidos

--- lib/xilofono.dart
+++ lib/xilofono.dart
@@ -11,60 +11,108 @@ class xilofono extends StatefulWidget {
 
 // ignore: camel_case_types
 class _xilofonoState extends State<xilofono> {
+  void sounds(int noteNumber) {
+    setState(() {
+      Audio.load('assets/sounds/note$noteNumber.wav')
+        ..play()
+        ..dispose();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note1.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note2.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note3.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note4.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note5.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note6.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press')),
-        TextButton(
-            onPressed: () {
-              Audio.load('assets/sounds/assets_note7.wav')
-                ..play()
-                ..dispose();
-            },
-            child: const Text('press'))
-      ],
-    );
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.red,
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(1);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.amber[600],
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(2);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.yellow,
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(3);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.lightGreen[400],
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(4);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.lightBlue[300],
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(5);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.blue,
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(6);
+          },
+          child: const Text(""),
+        ),
+      ))),
+      SizedBox(
+          child: Expanded(
+              child: Container(
+        color: Colors.purple[300],
+        height: MediaQuery.of(context).size.height / 7.83,
+        width: MediaQuery.of(context).size.width / 0.23,
+        child: TextButton(
+          onPressed: () {
+            sounds(7);
+          },
+          child: const Text(""),
+        ),
+      ))),
+    ]);
   }
 }
